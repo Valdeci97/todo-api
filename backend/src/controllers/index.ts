924c66c64e4ca3830ae75fc22c1a8eb42a547a58
valdeci <moura97.valdeci@gmail.com>
2022-10-31 22:41:43 -0300

    feat(controllers): create generic controller

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,59 @@
+import { Request, NextFunction, Response } from 'express';
+import HttpException from '../exceptions/HttpException';
+import { RequestWithBody } from '../interfaces/RequestWithBody';
+import Service from '../services';
+
+export default abstract class Controller<T> {
+  protected service: Service<T>;
+
+  protected readonly statusCode = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    NOT_FOUND: 404,
+  };
+
+  constructor(service: Service<T>) {
+    this.service = service;
+  }
+
+  public abstract create(
+    _req: RequestWithBody<T>,
+    _res: Response,
+    _next: NextFunction
+  ): any;
+
+  public read = async (
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<Array<Partial<T>>> | void> => {
+    try {
+      const obj = await this.service.read();
+      return res.status(this.statusCode.OK).json(obj);
+    } catch (err) {
+      if (err instanceof HttpException) {
+        return next(new HttpException(err.status, err.message));
+      }
+      next(new HttpException());
+    }
+  };
+
+  public abstract readOne(
+    _req: Request,
+    _res: Response,
+    _next: NextFunction
+  ): any;
+
+  public abstract update(
+    _req: RequestWithBody<T>,
+    _res: Response,
+    _next: NextFunction
+  ): any;
+
+  public abstract delete(
+    _req: Request,
+    _res: Response,
+    _next: NextFunction
+  ): any;
+}
