ba0d4564d814a0e361b00ca8a2cd7075e2d4f1c6
valdeci <moura97.valdeci@gmail.com>
2022-11-02 21:31:10 -0300

    feat(server): add login router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import App from './app';
 import userRouter from './routes/UserRouter';
+import loginRouter from './routes/LoginRouter';
 
 const server = new App();
 
 server.addRouter(userRouter.getRouter());
+server.addRouter(loginRouter.getRouter());
 
 server.addErrorMiddleware();
 
