42c388dc2750555086bdc2fae861724257881b6c
valdeci <moura97.valdeci@gmail.com>
2022-11-03 11:54:55 -0300

    feat(route): aplly user middleware

--- backend/src/routes/UserRouter.ts
+++ backend/src/routes/UserRouter.ts
@@ -1,12 +1,20 @@
 import CustomRouter from '.';
 import UserController from '../controllers/UserController';
+import UserMiddleware from '../middlewares/UserMiddleware';
 
 const userController = new UserController();
 const userRouter = new CustomRouter();
+const userMiddleware = new UserMiddleware();
 const userRoute = userController.getRoute();
 const userRouteWithIdParam = `${userController.getRoute()}/:id`;
 
-userRouter.addPostRoute(userRoute, userController.create);
+userRouter.addPostRoute(
+  userRoute,
+  userController.create,
+  userMiddleware.validateName,
+  userMiddleware.validateEmail,
+  userMiddleware.validatePassword
+);
 userRouter.addGetRoute(userRoute, userController.read);
 userRouter.addGetRoute(userRouteWithIdParam, userController.readOne);
 userRouter.addPatchRoute(userRouteWithIdParam, userController.update);
