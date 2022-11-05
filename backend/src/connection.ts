b6cebf28ff6f40d219661f754d4cd77c401b8541
valdeci <moura97.valdeci@gmail.com>
2022-11-05 16:36:55 -0300

    refactor(connection): update logger grammar

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -4,16 +4,14 @@ import logger from './logger';
 
 const MONGO_DB_URL = 'mongodb://root:docker@db/todo?authSource=admin';
 
-const connectToDatabase = async (
+export default async function connectToDatabase(
   mongoUrl = process.env.DATABASE_URL || MONGO_DB_URL
-): Promise<void> => {
+): Promise<void> {
   try {
     logger.info('Connecting to database');
     await mongoose.connect(mongoUrl);
     logger.info('Connected with success');
   } catch (err) {
-    logger.fatal(`Error to connect with database: ${err}`);
+    logger.fatal(`Error connecting to database: ${err}`);
   }
-};
-
-export default connectToDatabase;
+}
