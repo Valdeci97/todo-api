e32ded5f99c54d5729e98348391de9f016aadea7
valdeci <moura97.valdeci@gmail.com>
2022-11-05 15:27:05 -0300

    feat(server): add task router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import App from './app';
 import userRouter from './routes/UserRouter';
 import loginRouter from './routes/LoginRouter';
+import taskRouter from './routes/TaskRouter';
 
 const server = new App();
 
 server.addRouter(userRouter.getRouter());
 server.addRouter(loginRouter.getRouter());
+server.addRouter(taskRouter.getRouter());
 
 server.addErrorMiddleware();
 
