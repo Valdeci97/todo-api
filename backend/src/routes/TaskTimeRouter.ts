e934586384e52ef6b383c1ec0831696f5120ddfd
valdeci <moura97.valdeci@gmail.com>
2022-11-06 21:19:38 -0300

    feat(routes): add middlewares to task time router

--- backend/src/routes/TaskTimeRouter.ts
+++ backend/src/routes/TaskTimeRouter.ts
@@ -1,8 +1,12 @@
 import CustomRouter from '.';
 import TaskTimeController from '../controllers/TaskTimeController';
+import GuidMiddleware from '../middlewares/GuidMiddleware';
+import TokenMiddleware from '../middlewares/TokenMiddleware';
 
 const taskTimeRouter = new CustomRouter();
 const taskTimeController = new TaskTimeController();
+const tokenMiddleware = new TokenMiddleware();
+const guidMiddleware = new GuidMiddleware();
 const taskRoute = taskTimeController.getRoute();
 const dayRoute = `${taskRoute}/day/:id`;
 const weekRoute = `${taskRoute}/week/:id`;
@@ -10,10 +14,39 @@ const monthRoute = `${taskRoute}/month/:id`;
 const yearRoute = `${taskRoute}/year/:id`;
 const lateRoute = `${taskRoute}/late/:id`;
 
-taskTimeRouter.addGetRoute(dayRoute, taskTimeController.todayTasks);
-taskTimeRouter.addGetRoute(weekRoute, taskTimeController.weekTasks);
-taskTimeRouter.addGetRoute(monthRoute, taskTimeController.monthTasks);
-taskTimeRouter.addGetRoute(yearRoute, taskTimeController.yearTasks);
-taskTimeRouter.addGetRoute(lateRoute, taskTimeController.lateTasks);
+taskTimeRouter.addGetRoute(
+  dayRoute,
+  taskTimeController.todayTasks,
+  guidMiddleware.validateGuid,
+  tokenMiddleware.validate
+);
+
+taskTimeRouter.addGetRoute(
+  weekRoute,
+  taskTimeController.weekTasks,
+  guidMiddleware.validateGuid,
+  tokenMiddleware.validate
+);
+
+taskTimeRouter.addGetRoute(
+  monthRoute,
+  taskTimeController.monthTasks,
+  guidMiddleware.validateGuid,
+  tokenMiddleware.validate
+);
+
+taskTimeRouter.addGetRoute(
+  yearRoute,
+  taskTimeController.yearTasks,
+  guidMiddleware.validateGuid,
+  tokenMiddleware.validate
+);
+
+taskTimeRouter.addGetRoute(
+  lateRoute,
+  taskTimeController.lateTasks,
+  guidMiddleware.validateGuid,
+  tokenMiddleware.validate
+);
 
 export default taskTimeRouter;
