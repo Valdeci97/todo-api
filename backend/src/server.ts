4a3d163cda19ffb592e961079c27ffa3ede8c6b7
valdeci <moura97.valdeci@gmail.com>
2022-11-06 21:15:35 -0300

    feat(server): add task time router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import App from './app';
 import userRouter from './routes/UserRouter';
 import loginRouter from './routes/LoginRouter';
 import taskRouter from './routes/TaskRouter';
+import taskTimeRouter from './routes/TaskTimeRouter';
 
 const server = new App();
 
 server.addRouter(userRouter.getRouter());
 server.addRouter(loginRouter.getRouter());
 server.addRouter(taskRouter.getRouter());
+server.addRouter(taskTimeRouter.getRouter());
 
 server.addErrorMiddleware();
 
