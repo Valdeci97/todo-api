c7ae5a818abd41b275d3c2c26cfa9267fbcfb30c
valdeci <moura97.valdeci@gmail.com>
2022-11-17 14:57:22 -0300

    refactor(vercel): try to solve vercel problems change db variable name

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -2,14 +2,13 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import logger from './logger';
 
-const MONGO_DB_URL = 'mongodb://root:docker@db/todo?authSource=admin';
+const MONGODB_URL = 'mongodb://root:docker@db/todo?authSource=admin';
+const MONGODB_URI = process.env.MONGODB_URI || MONGODB_URL;
 
-export default async function connectToDatabase(
-  mongoUrl = process.env.DATABASE_URL || MONGO_DB_URL
-): Promise<void> {
+export default async function connectToDatabase(): Promise<void> {
   try {
     logger.info('Connecting to database');
-    await mongoose.connect(mongoUrl);
+    await mongoose.connect(MONGODB_URI);
     logger.info('Connected with success');
   } catch (err) {
     logger.fatal(`Error connecting to database: ${err}`);
