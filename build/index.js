e9dd42ab7eade25f8ebd501d78c789afdde0cbbc
valdeci <moura97.valdeci@gmail.com>
2022-11-18 22:39:36 -0300

    build(heroku): use babel as dependency

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-require("dotenv/config");
-var _server = _interopRequireDefault(require("./server"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const PORT = process.env.PORT || 3001;
-_server.default.start(PORT);
